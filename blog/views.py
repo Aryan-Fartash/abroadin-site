45cbe620a655ae435f5acb15d1b979fe29651961
Aryan-Fartash <fartasharyan@gmail.com>
2024-10-05 00:03:17 -0600

    trying to load the pages
    trying to load the pages

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def canada_indian_students_view(request):
     return render(request,'blog/best-canadian-universities-for-indian-students.html')
 
 def test_view(request):
-    return render(request,'base.html')
+    return render(request,'blog/test.html')
 
 def master_europe_view(request):
     return render(request,'blog/masters-programs-in-europe.html')
